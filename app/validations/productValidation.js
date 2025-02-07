195244e8b510216c7f9fb820c4dd836eff9f6225
nikulShivInfoTech <nikulkumar.shivinfotech@gmail.com>
2025-02-07 18:33:40 +0530

    added empty validation

--- app/validations/productValidation.js
+++ app/validations/productValidation.js
@@ -1,9 +1,9 @@
 const Joi = require('joi');
 
 const productValidation = Joi.object({
-  product_name: Joi.string().min(3).max(50).required().messages({
+  product_name: Joi.string().min(3).max(50).required().empty('').messages({
     'string.base': 'Product name must be a string.',
-    'string.empty': 'Product name is required.',
+    'string.empty': 'Product name cannot be empty.',
     'string.min': 'Product name must be at least 3 characters long.',
     'string.max': 'Product name cannot exceed 50 characters.',
     'any.required': 'Product name is required.',
@@ -14,12 +14,15 @@ const productValidation = Joi.object({
     'any.required': 'Category ID is required.',
   }),
 });
-const productCommon= Joi.object({
-  product_name: Joi.string().min(3).max(50).required().messages({
+
+const productCommon = Joi.object({
+  product_name: Joi.string().min(3).max(50).required().empty('').messages({
     'string.base': 'Product name must be a string.',
-    'string.empty': 'Product name is required.',
+    'string.empty': 'Product name cannot be empty.',
     'string.min': 'Product name must be at least 3 characters long.',
     'string.max': 'Product name cannot exceed 50 characters.',
     'any.required': 'Product name is required.',
-  }),});
+  }),
+});
+
 module.exports = { productValidation, productCommon };
